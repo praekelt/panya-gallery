4b08bfc027dd460feb3fe9a0eb79efc9bee87048
Shaun Sephton <shaun@praekelt.com>
2010-04-20 10:22:37 +0200

    don't set own fieldsets

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,3 @@
-from copy import deepcopy
-
 from django.contrib import admin
 
 from content.admin import ModelBaseAdmin
@@ -7,13 +5,8 @@ from content.admin import ModelBaseAdmin
 from gallery.models import Gallery, GalleryImage, VideoEmbed, VideoFile
 
 class GalleryImageAdmin(ModelBaseAdmin):
-    fieldsets = deepcopy(ModelBaseAdmin.fieldsets)
     list_display = ModelBaseAdmin.list_display + ('gallery',)
     list_filter = ModelBaseAdmin.list_filter + ('gallery',)
-    
-    for fieldset in fieldsets:
-        if fieldset[0] == None:
-            fieldset[1]['fields'] += ('gallery',)
 
 admin.site.register(Gallery, ModelBaseAdmin)
 admin.site.register(GalleryImage, GalleryImageAdmin)
