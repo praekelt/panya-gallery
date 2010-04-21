c66c2ac34fe9cb774bd789b4e4f88268bd2fc4bf
Shaun Sephton <shaun@praekelt.com>
2010-04-21 15:12:53 +0200

    gallery detail url

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'gallery.views',
-    url(r'^listing/$', 'object_listing', name='gallery_object_listing'),
+    url(r'^listing$', 'object_listing', name='gallery_object_listing'),
     url(r'^(?P<slug>[\w-]+)/$', 'object_detail', name='gallery_object_detail'),
 )
