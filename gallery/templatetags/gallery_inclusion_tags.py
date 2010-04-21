a26915cdad9684db9acd281857759258d48362bf
Shaun Sephton <shaun@praekelt.com>
2010-04-21 15:13:18 +0200

    gallery detail tag

--- gallery/templatetags/gallery_inclusion_tags.py
+++ gallery/templatetags/gallery_inclusion_tags.py
@@ -2,6 +2,10 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('gallery/inclusion_tags/gallery_listing.html')
+@register.inclusion_tag('gallery/inclusion_tags/gallery_list.html')
 def gallery_listing(object_list):
     return {'object_list': object_list}
+
+@register.inclusion_tag('gallery/inclusion_tags/gallery_detail.html')
+def gallery_detail(object):
+    return {'object': object}
