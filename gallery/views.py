c4ebcebd80e48a45f94641657d13df467c8d5ca6
Shaun Sephton <shaun@praekelt.com>
2010-04-22 17:22:31 +0200

    corrected extra context moethod

--- gallery/views.py
+++ gallery/views.py
@@ -3,10 +3,20 @@ from gallery.models import Gallery
 
 class ObjectList(GenericObjectList):
     def get_queryset(self):
+        
         return Gallery.permitted.all()
 
-    def get_extra_context(self):
-        return {'title': 'Galleries'}
+    def get_extra_context(self, *args, **kwargs):
+        extra_context = super(ObjectList, self).get_extra_context(*args, **kwargs)
+        added_context = {'title': 'Galleries'}
+        if extra_context:
+            extra_context.update(
+                added_context,
+            )
+        else:
+            extra_context = added_context
+
+        return extra_context
 
 object_list = ObjectList()
 
