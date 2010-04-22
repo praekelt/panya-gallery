79e7bc131db4f3cd0bad9bba39f69aef6f4c16e3
Shaun Sephton <shaun@praekelt.com>
2010-04-22 17:23:03 +0200

    corrected url list url

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(
     'gallery.views',
-    url(r'^listing/$', 'object_list', name='gallery_object_listing'),
+    url(r'^list/$', 'object_list', name='gallery_object_list'),
     url(r'^(?P<slug>[\w-]+)/$', 'object_detail', name='gallery_object_detail'),
 )
