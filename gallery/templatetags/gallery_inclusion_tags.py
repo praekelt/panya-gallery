4858d1315f25738987034e818375debb1b823a0d
Shaun Sephton <shaun@praekelt.com>
2010-04-30 16:27:53 +0200

    object -> obj

--- gallery/templatetags/gallery_inclusion_tags.py
+++ gallery/templatetags/gallery_inclusion_tags.py
@@ -7,5 +7,5 @@ def gallery_listing(object_list):
     return {'object_list': object_list}
 
 @register.inclusion_tag('gallery/inclusion_tags/gallery_detail.html')
-def gallery_detail(object):
-    return {'object': object}
+def gallery_detail(obj):
+    return {'object': obj}
