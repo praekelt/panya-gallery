f6b4e161e4c43604800587c3d9d1d3b520765772
Shaun Sephton <shaun@praekelt.com>
2010-04-30 16:27:27 +0200

    ajax url

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns(
     'gallery.views',
     url(r'^list/$', 'object_list', name='gallery_object_list'),
     url(r'^(?P<slug>[\w-]+)/$', 'object_detail', name='gallery_object_detail'),
+    url(r'^item/ajax/galleriffic/(?P<slug>[\w-]+)/$', 'gallery_item_ajax_galleriffic', name='gallery_item_ajax_galleriffic'),
 )
