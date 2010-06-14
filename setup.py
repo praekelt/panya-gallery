93d25b780ee42bd67b4c6d7d1aa20571a2d26a24
shaunsephton <shaunsephton@gmail.com>
2010-06-14 13:58:50 +0200

    panya rename

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-gallery',
-    version='dev',
-    description='Django gallery app.',
-    author='Praekelt Consulting',
+    name='panya-gallery',
+    version='0.0.1',
+    description='Panya gallery app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
     author_email='dev@praekelt.com',
-    url='https://github.com/praekelt/django-gallery',
+    license='BSD',
+    url='http://github.com/praekelt/panya-gallery',
     packages = find_packages(),
+    install_requires = [
+        'panya',
+    ],
     include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Panya",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
